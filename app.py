32cfa01793b50bc2a56ad7d9c3f41a4a5f9d6a69
JaedenHufnagle <54830020+JaedenHufnagle@users.noreply.github.com>
2020-11-24 03:51:28 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,59 +1,25 @@
-from flask import Flask, render_template,request, redirect, url_for
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
-from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from wtforms import validators
+from wtforms.validators import Required
+from wtforms.fields.html5 import EmailField, DateTimeLocalField
 
 app = Flask(__name__)
-application = app
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///ingredients.db'
-
-db=SQLAlchemy(app)
-
-migrate = Migrate(app, db)
-
-class Ingredients(db.Model):
-    _tablename_='ingredients'
-    id = db.Column(db.Integer, primary_key=True)
-    ingredient = db.Column(db.String(200),nullable=False)
-
-    def _repr_ (self):
-        return '<Ingredient %r>' % self.ingredient
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db, Ingredients=Ingredients)
-
-class ItemForm(FlaskForm):
-    item = StringField('Add a new item:', validators=[DataRequired()])
-    submit = SubmitField('Add Item!')
-
-class DeleteForm(FlaskForm):
-    item = StringField('Enter the item you wish to delete', validators=[DataRequired()])
-    submit = SubmitField('Remove item')
-
-class EditForm(FlaskForm):
-    inital = StringField('Enter the item you wish to change', validators=[DataRequired()])
-    change = StringField('Enter the items new information', validators=[DataRequired()])
-    submit = SubmitField('Change item')
-
-class ModeForm(FlaskForm):
-    options=[('edit','Edit Mode'),('delete','Delete Mode'), ('add','Add Mode')]
-    mode = SelectField(u'View', choices=options)
-    submit = SubmitField('Change Mode')
-
-class EmptyForm(FlaskForm):
-    empty= StringField('Click one of the modes above to get started')
-
+class ProfileForm(FlaskForm):
+    name = StringField('Name:', validators=[validators.DataRequired()])
+    username = StringField('Username:', validators=[validators.DataRequired()])
+    email = EmailField('Email:', [validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password:')
+    submit = SubmitField('Sign up')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -64,102 +30,16 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/add',methods= ['GET','POST'])
-def add_index():
-    form = ItemForm()
-    mode = ModeForm()
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
 
-    
-    if mode.is_submitted():
-        if(mode.mode.data=='delete'):
-             return redirect(url_for('delete_index'))
-        elif(mode.mode.data=='add'):
-            return redirect(url_for('add_index'))
-        elif(mode.mode.data=='edit'):
-            return redirect(url_for('edit_index'))
-
-    if form.is_submitted():
-        ingredient = Ingredients.query.filter_by(ingredient=form.item.data).first()
-        if ingredient is None:
-            ingredient= Ingredients(ingredient=form.item.data)
-            db.session.add(ingredient)
-            db.session.commit()
-
-    form.item.data=None
-    ingredients=db.session.query(Ingredients).filter_by().all()
-    return render_template('index.html' ,Ingredients=ingredients, form=form,mode=mode)
-
-@app.route('/edit',methods= ['GET','POST'])
-def edit_index():
-    form=EditForm()
-    mode = ModeForm()
-
-    
-    if mode.is_submitted():
-        if(mode.mode.data=='delete'):
-             return redirect(url_for('delete_index'))
-        elif(mode.mode.data=='add'):
-            return redirect(url_for('add_index'))
-        elif(mode.mode.data=='edit'):
-            return redirect(url_for('edit_index'))
-
-    if form.is_submitted():
-        initial=form.inital.data
-        result=db.session.query(Ingredients).filter_by(ingredient=initial).first()
-        if result is not None:
-            db.session.delete(result)
-            change=form.change.data
-            ingredient=Ingredients(ingredient=change)
-            db.session.add(ingredient)
-            db.session.commit()
-
-    form.change.data=None
-    form.inital.data=None
-    ingredients=db.session.query(Ingredients).filter_by().all()
-
-    return render_template('index.html' ,Ingredients=ingredients, form=form, mode=mode)
-
-
-@app.route('/delete',methods=['GET','POST'])
-def delete_index():
-    form = DeleteForm()
-    mode = ModeForm()
-
-    
-    if mode.is_submitted():
-        if(mode.mode.data=='delete'):
-             return redirect(url_for('delete_index'))
-        elif(mode.mode.data=='add'):
-            return redirect(url_for('add_index'))
-        elif(mode.mode.data=='edit'):
-            return redirect(url_for('edit_index'))
-
-    if form.is_submitted():
-        result=db.session.query(Ingredients).filter_by(ingredient=form.item.data).first()
-        if result is not None:
-            db.session.delete(result)
-            db.session.commit()
-            
-    form.item.data=None
-    ingredients=db.session.query(Ingredients).filter_by().all()
-    return render_template('index.html', Ingredients=ingredients,form=form, mode=mode)
-
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = EmptyForm()
-    mode = ModeForm()
-
-    if mode.is_submitted():
-        if(mode.mode.data=='delete'):
-             return redirect(url_for('delete_index'))
-        elif(mode.mode.data=='add'):
-            return redirect(url_for('add_index'))
-        elif(mode.mode.data=='edit'):
-            return redirect(url_for('edit_index'))
-
-
-    ingredients=db.session.query(Ingredients).filter_by().all()
-
-    return render_template('index.html', Ingredients=ingredients, mode=mode, form=form)
+@app.route('/signup')
+def singup():
+    form = ProfileForm()
+    #ingredient = None
+    #form = NameForm()
+    #if form.validate_on_submit():
+       # ingredient = form.ingredient.data
+       # form.ingredient.data = ''
+    return render_template('signup.html',form=form)
