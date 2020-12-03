abbe4d27a4581eaacfc8ba867d0159a96e1bb530
segamale <74773976+segamale@users.noreply.github.com>
2020-12-03 16:12:59 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+#class NameForm(FlaskForm):
+    #ingredient = StringField('Enter a new item?', validators=[DataRequired()])
+    #submit = SubmitField('Add it!')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/')
+def homepage():
+    #ingredient = None
+    #form = NameForm()
+    #if form.validate_on_submit():
+       # ingredient = form.ingredient.data
+       # form.ingredient.data = ''
+    return render_template('homepage.html')
+
+@app.route('/signup.html')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/signin.html')
+def signin():
+    return render_template('signin.html')
+
